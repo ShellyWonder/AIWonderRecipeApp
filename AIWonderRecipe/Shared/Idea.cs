de1d043885cdf998c557f141c3c112ba8c84fac6
ShellyWonder <Shelly.Wonder@outlook.com>
2023-10-26 17:12:41 -0500

    built functionality for the MealIdeas component

---
+++ AIWonderRecipe/Shared/Idea.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIWonderRecipe.Shared
+{
+    public class Idea
+    {
+        public int index {  get; set; }
+        public string? title { get; set; }
+        public string? description { get; set; }
+    }
+}
