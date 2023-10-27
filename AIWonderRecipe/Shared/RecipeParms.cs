82241f65959c32bfdf70c4769db5d36a87bf1205
ShellyWonder <Shelly.Wonder@outlook.com>
2023-10-27 13:45:22 -0500

    created new shared class, RecipeParms.cs

---
+++ AIWonderRecipe/Shared/RecipeParms.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIWonderRecipe.Shared
+{
+    public class RecipeParms
+    {
+        public string MealTime { get; set; } = "Breakfast";
+        public List<Ingredient> Ingredients { get; set;} = new List<Ingredient> { };
+        public string? SelectedIdea { get; set; }
+    }
+}
