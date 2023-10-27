60272a0123bfbd7f0be7f330ec4d529242d9b6ce
ShellyWonder <Shelly.Wonder@outlook.com>
2023-10-27 17:32:24 -0500

    Enabling the API-related logic

---
+++ AIWonderRecipe/Server/Controllers/RecipeController.cs
@@ -0,0 +1,21 @@
+﻿
+using AIWonderRecipe.Server.Data;
+using AIWonderRecipe.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace AIWonderRecipe.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RecipeController : ControllerBase
+    {
+        [HttpPost, Route("GetRecipeIdeas")]
+        public async Task<ActionResult<List<Idea>>> GetRecipeIdeas(RecipeParms recipeParms)
+        {
+
+            return SampleData.RecipeIdeas;
+        }
+    }
+}
