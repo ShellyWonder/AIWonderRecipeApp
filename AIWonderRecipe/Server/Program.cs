f1b8570fdb7021feea9322bc308b094c43709170
ShellyWonder <Shelly.Wonder@outlook.com>
2023-10-29 09:20:23 -0500

    Registered service with (server)program.cs

--- AIWonderRecipe/Server/Program.cs
+++ AIWonderRecipe/Server/Program.cs
@@ -1,3 +1,4 @@
+using AIWonderRecipe.Server.Services;
 using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddScoped<IOpenAIAPI, OpenAIAPIService>();
 
 var app = builder.Build();
 
