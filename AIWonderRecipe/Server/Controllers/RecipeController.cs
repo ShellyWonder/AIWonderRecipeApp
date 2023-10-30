015cc901ae966b22829dfe533f5c0701ca659101
ShellyWonder <Shelly.Wonder@outlook.com>
2023-10-29 20:32:22 -0500

    Building the idea's prompt code for the API

--- AIWonderRecipe/Server/Controllers/RecipeController.cs
+++ AIWonderRecipe/Server/Controllers/RecipeController.cs
@@ -1,5 +1,6 @@
 ﻿
 using AIWonderRecipe.Server.Data;
+using AIWonderRecipe.Server.Services;
 using AIWonderRecipe.Shared;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -10,12 +11,33 @@ namespace AIWonderRecipe.Server.Controllers
     [Route("[controller]")]
     [ApiController]
     public class RecipeController : ControllerBase
-    {
+    { 
+        private readonly IOpenAIAPI _openAIService;
+
+        public RecipeController(IOpenAIAPI openAIService)
+        {
+            _openAIService = openAIService;
+        }
+
         [HttpPost, Route("GetRecipeIdeas")]
         public async Task<ActionResult<List<Idea>>> GetRecipeIdeas(RecipeParms recipeParms)
         {
+            //string mealtime = recipeParms.MealTime;
+            //List<string> ingredients = recipeParms.Ingredients
+            //                                      .Where(x => !string.IsNullOrEmpty(x.Description))
+            //                                      .Select(x => x.Description!)
+            //                                      .ToList();
+            //if (string.IsNullOrEmpty(mealtime))
+            //{
+            //    mealtime = "Breakfast";
+            //}
+            //var ideas = await _openAIService.CreateRecipeIdeas(mealtime, ingredients);
 
+            //return ideas;
+
+            //Testing purposes only
             return SampleData.RecipeIdeas;
         }
     }
 }
+
--- AIWonderRecipe/Server/Services/OpenAIAPIService.cs
+++ AIWonderRecipe/Server/Services/OpenAIAPIService.cs
@@ -1,6 +1,10 @@
 ﻿using AIWonderRecipe.Server.Models.ChatEndPoint;
+using System.Net.Http.Headers;
 using AIWonderRecipe.Shared;
 using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Http;
 
 namespace AIWonderRecipe.Server.Services
 {
@@ -10,7 +14,7 @@ namespace AIWonderRecipe.Server.Services
         private static readonly string _baseUrl = "https://api.openai.com/v1/";
         private static readonly HttpClient _httpClient = new();
         private readonly JsonSerializerOptions? _jsonOptions;
-       
+
         //build the function objects that will return JSON formatted object from the API
         private static ChatFunction.Parameter _recipeIdeaParameter = new()
         {
@@ -61,11 +65,89 @@ namespace AIWonderRecipe.Server.Services
             }
         };
 
-        
-
-        public Task<List<Idea>> CreateRecipeIdeas(string mealtime, List<string> ingredients)
+        public OpenAIAPIService(IConfiguration configuration)
         {
-            throw new NotImplementedException();
+            _configuration = configuration;
+            var apiKey = _configuration["OpenAI:APIKey"] ?? Environment.GetEnvironmentVariable("OpenAiKey");
+
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _httpClient.DefaultRequestHeaders.Authorization = new("Bearer", apiKey);
+
+            _jsonOptions = new JsonSerializerOptions
+
+            {
+                PropertyNameCaseInsensitive = true,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+            };
         }
+
+
+        public async Task<List<Idea>> CreateRecipeIdeas(string mealtime, List<string> ingredientList)
+        {
+            string url = $"{_baseUrl}chat/completions";
+            string systemPrompt = "You are a world-renowned chef. I will send you a list of ingredients and a meal time. You will respond with 5 ideas for dishes.";
+            string userPrompt = "";
+            string ingredientPrompt = "";
+
+
+            string ingredients = string.Join(",", ingredientList);
+            if (string.IsNullOrEmpty(ingredients))
+            {
+                ingredientPrompt = "Suggest some ingredients for me.";
+            }
+            else
+            {
+                ingredientPrompt = $"I have{ingredients}";
+            }
+            userPrompt = $"The meal I want to cook is {mealtime}.{ingredientPrompt}";
+            ChatMessage systemMessage = new()
+            {
+                Role = "system",
+                Content = $"{systemPrompt}"
+            };
+            ChatMessage userMessage = new()
+            {
+                Role = "user",
+                Content = $"{userPrompt}"
+            };
+            ChatRequest request = new()
+            {
+                Model = "gpt-3.5-turbo-0613",
+                Messages = new[] { systemMessage, userMessage },
+                Functions = new[] { _ideaFunction },
+                FunctionCall = new { Name = _ideaFunction.Name }
+
+            };
+            HttpResponseMessage httpResponse = await _httpClient.PostAsJsonAsync(url, request, _jsonOptions);
+
+            ChatResponse? response = await httpResponse.Content.ReadFromJsonAsync<ChatResponse>();
+            //get function's first message
+            ChatFunctionResponse? functionResponse = response?.Choices?
+                                                     .FirstOrDefault(m => m.Message?.FunctionCall is not null)?
+                                                     .Message?
+                                                     .FunctionCall;
+            Result<List<Idea>>? ideasResult = new();
+            if (functionResponse?.Arguments is not null)
+            {
+                try
+                {
+                    ideasResult = JsonSerializer.Deserialize<Result<List<Idea>>>(functionResponse.Arguments, _jsonOptions);
+
+                }
+                catch (Exception ex)
+                {
+
+                    ideasResult = new()
+                    {
+                        Exception = ex,
+                        ErrorMessage = await httpResponse.Content.ReadAsStringAsync()
+                    };
+                }
+            }
+            return ideasResult?.Data?? new List<Idea>();
+        }
+
     }
 }
+
