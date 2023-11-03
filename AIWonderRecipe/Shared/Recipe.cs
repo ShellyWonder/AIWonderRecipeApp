bd456b728c9555fa9c95b5d7222598b94b907d99
ShellyWonder <Shelly.Wonder@outlook.com>
2023-11-03 14:32:20 -0500

    created recipe class

---
+++ AIWonderRecipe/Shared/Recipe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIWonderRecipe.Shared
+{
+    public class Recipe
+    {
+        public string? Title { get; set; }
+        public string[]? Ingredients { get; set; }
+        public string[]? Instructions { get; set; }
+        public string? Summary { get; set; }
+
+        public static implicit operator Recipe(Recipe v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
