f10013077b7cc268dd8873d54e6b0629e37de311
ShellyWonder <Shelly.Wonder@outlook.com>
2023-11-03 14:31:41 -0500

    added sample data 4 testing

--- AIWonderRecipe/Server/Data/SampleData.cs
+++ AIWonderRecipe/Server/Data/SampleData.cs
@@ -33,6 +33,43 @@ namespace AIWonderRecipe.Server.Data
                 Description = "Ginger snaps are a classic favorite. With just a few ingredients and even fewer steps this recipe for fabulous, spicy cookies is truly a snap to make."
             },
         };
+
+        public static Recipe Recipe = new()
+        {
+            Title = "Ginger Snaps",
+            Summary = "Ginger snaps are a classic favorite. With just a few ingredients and even fewer steps this recipe for fabulous, spicy cookies is truly a snap to make.",
+            Ingredients = new[]
+          {
+                "1 cup packed brown sugar",
+                "3/4 cup vegetable oil",
+                "1/4 cup molasses",
+                "1 large egg",
+                "2 cups all-purpose flour",
+                "2 teaspoons baking soda",
+                "1 teaspoon ground ginger",
+                "1 teaspoon ground cinnamon",
+                "3/4 teaspoon ground cloves",
+                "1/4 teaspoon salt",
+            },
+            Instructions = new[]
+          {
+                "Preheat the oven to 375 degrees F (190 degrees C).",
+                "Mix together brown sugar, oil, molasses, and egg in a large bowl.",
+                "Combine flour, baking soda, ginger, cinnamon, cloves, and salt; stir into the molasses mixture.",
+                "Roll dough into 1 1/4-inch balls. Roll each ball in white sugar before placing 2 inches apart on ungreased cookie sheets.",
+                "Bake for 10 to 12 minutes in the preheated oven, or until center is firm. Cool on wire racks.",
+            }
+        };
+
+        public static RecipeImage RecipeImage = new()
+        {
+            Data = new ImageData[] {
+                new ImageData()
+                {
+                    Url = "https://www.allrecipes.com/thmb/nX1dNGl1mKTUOLCKTkRmhPkdohE=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/5969369-682611ebae5a42ddbcaaa90c27b3e1fd.jpg"
+                }
+            }
+        };
+
     }
 }
-
